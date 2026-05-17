1fa84dbc97179d04e01ceae01089eef3ca7baf39
Romeo Esty <re283@kent.ac.uk>
2026-05-17 20:59:18 +0100

    fix: cleaned up code

--- extract.py
+++
@@ -1,8 +0,0 @@
-import json
-import sqlite3
-
-import json
-
-with open('data/integrated.json') as f:
-    d = json.load(f)
-    print(d)
--- sqlite.py
+++ sqlite.py
@@ -4,41 +4,29 @@ connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
 
 cursor = connection.cursor()
 
-sql = """create table if not exists locations(
+cursor.execute( """create table if not exists locations(
     ID integer primary key autoincrement,
     lat real,
     lon real,
     operator_reference text,
     country_reference text,
     postal_code text,
-    number_of_evses integer
-    )"""
+    number_of_evses integer)
+    """)
+    
 
-cursor.execute(sql)
-
-
-connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
-
-cursor = connection.cursor()
-
-sql_1 = """create table if not exists eves(
+cursor.execute( """create table if not exists eves(
     ID integer primary key autoincrement,
     physical_identifer text,
-    status text
-    )"""
+    status text)
+    """)
 
-cursor.execute(sql_1)
-
-connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
-
-cursor = connection.cursor()
-
-sql_2 = """create table if not exists connectors(
+cursor.execute( """create table if not exists connectors(
     ID integer primary key autoincrement,
 
     standard text,
-    power integer
-    )"""
+    power integer)
+    """)
 
-cursor.execute(sql_2)
-
+connection.commit()
+connection.close()
