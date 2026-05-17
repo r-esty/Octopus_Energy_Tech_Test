779bfdd256459daab51dea401c616828532efb0d
Romeo Esty <re283@kent.ac.uk>
2026-05-17 18:47:54 +0100

    feat: Add eves and connectors table

--- sqlite.py
+++ sqlite.py
@@ -4,7 +4,7 @@ connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
 
 cursor = connection.cursor()
 
-sql = """create table if not exist locations(
+sql = """create table locations(
     ID integer primary key autoincrement,
     lat real,
     ion real,
@@ -15,3 +15,30 @@ sql = """create table if not exist locations(
     )"""
 
 cursor.execute(sql)
+
+
+connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
+
+cursor = connection.cursor()
+
+sql_1 = """create table eves(
+    ID integer primary key autoincrement,
+    physical_identifer text,
+    status text
+    )"""
+
+cursor.execute(sql_1)
+
+connection = sqlite3.connect("/home/romeo/Octopus_Tech_Test/locations.db")
+
+cursor = connection.cursor()
+
+sql_2 = """create table connectors(
+    ID integer primary key autoincrement,
+
+    standard text,
+    power integer
+    )"""
+
+cursor.execute(sql_2)
+
