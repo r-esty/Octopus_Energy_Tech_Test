69f1127ae67b87e007f27742ccb27a91d95e2ac1
Romeo Esty <re283@kent.ac.uk>
2026-05-18 02:23:20 +0100

    feat: Add foreign keys to the sqlite.py

--- sqlite.py
+++ sqlite.py
@@ -10,20 +10,23 @@ cursor.execute("""create table if not exists locations(
     lon real,
     operator_reference text,
     country_reference text,
-    postal_code text,
-    number_of_evses integer)
+    postal_code text)
     """)
 
 cursor.execute("""create table if not exists evses(
     ID integer primary key autoincrement,
+    location_id integer,
     physical_identifier text,
-    status text)
+    status text,
+    foreign key (location_id) references locations(ID))
     """)
 
 cursor.execute("""create table if not exists connectors(
     ID integer primary key autoincrement,
+    evse_id integer,
     standard text,
-    power integer)
+    power integer,
+    foreign key (evse_id) references evses(ID))
     """)
 
 with open('data/integrated.json') as f:
@@ -47,15 +50,15 @@ for location in locations:
 
     for evse in location["evses"]:
         cursor.execute(
-            "INSERT INTO evses (physical_identifier, status) VALUES (?, ?)",
-            (evse["physical_reference"], evse["status"])
+            "INSERT INTO evses (location_id, physical_identifier, status) VALUES (?, ?, ?)",
+            (location_id, evse.get("physical_reference", ""), evse.get("status", ""))
         )
         evse_id = cursor.lastrowid
 
         for connector in evse["connectors"]:
             cursor.execute(
-                "INSERT INTO connectors (standard, power) VALUES (?, ?)",
-                (connector.get("standard", ""), connector.get("max_electric_power", 0))
+                "INSERT INTO connectors (evse_id, standard, power) VALUES (?, ?, ?)",
+                (evse_id, connector.get("standard", ""), connector.get("max_electric_power", 0))
             )
 
 connection.commit()
